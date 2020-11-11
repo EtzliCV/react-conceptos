7062eb05313f775e0ea5b4a5e1442f9f0e11b81f
Esly <eslysarhaycv1703@gmail.com>
2020-11-11 13:12:37 -0500

    cambio

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,8 +3,7 @@ function App() {
   return (
     <section className="bg-dark text-light">
       <div className="container py-5">
-        <h2 className="mb-4
-        ">Conceptos básicos de React</h2>
+        <h2 className="mb-4">Conceptos básicos de React</h2>
         <div className="row">
           <div className="col-sm-12 col-md-6 col-lg-3 col-xl-3 mb-4">
             <article className="bg-info p-3">
